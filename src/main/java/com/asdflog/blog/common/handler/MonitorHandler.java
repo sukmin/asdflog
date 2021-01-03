0434baa4fdfc8d67383f9a7894f89ef21913cf35
killroad <killroad@gmail.com>
2021-01-04 00:23:37 +0900

    l7check URL 추가
    - router 설정을 config 패키지 하위로 이동
    - l7check URL 추가
    - 로그파일 뒤에 .log가 붙도록 수정
    - 빌드 산출물 이름이 asdfblog로 잘못되어 있던 것 수정

--- src/main/java/com/asdflog/blog/common/handler/MonitorHandler.java
+++ src/main/java/com/asdflog/blog/common/handler/MonitorHandler.java
@@ -1,17 +1,20 @@
-package com.asdflog.blog.sample;
+package com.asdflog.blog.common.handler;
 
+import org.springframework.http.CacheControl;
 import org.springframework.http.MediaType;
-import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
 
-@Component
-public class SampleHandler {
+public class MonitorHandler {
 
-  public Mono<ServerResponse> hello(final ServerRequest request) {
+  private static final String OK = "OK";
+
+  public static Mono<ServerResponse> ok(final ServerRequest request) {
     return ServerResponse.ok().contentType(MediaType.TEXT_PLAIN)
-        .body(BodyInserters.fromValue("Hello, bambi!"));
+        .cacheControl(CacheControl.noStore())
+        .body(BodyInserters.fromValue(OK));
   }
+
 }
--- src/main/java/com/asdflog/blog/config/RouterConfig.java
+++ src/main/java/com/asdflog/blog/config/RouterConfig.java
@@ -1,5 +1,7 @@
-package com.asdflog.blog.sample;
+package com.asdflog.blog.config;
 
+import com.asdflog.blog.common.handler.MonitorHandler;
+import com.asdflog.blog.sample.SampleHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
@@ -9,13 +11,19 @@ import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
 @Configuration
-public class SampleRouter {
+public class RouterConfig {
 
   @Bean
   public RouterFunction<ServerResponse> route(final SampleHandler sampleHandler) {
-
     return RouterFunctions
         .route(RequestPredicates.GET("/hello")
             .and(RequestPredicates.accept(MediaType.TEXT_PLAIN)), sampleHandler::hello);
   }
+
+  @Bean
+  public RouterFunction<ServerResponse> monitorRouter() {
+    return RouterFunctions
+        .route(RequestPredicates.GET("/monitor/l7check"), MonitorHandler::ok);
+  }
+
 }
